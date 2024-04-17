05258155509fc1ec8a1d1e1f0c8f25055e01409c
PrincessLaSwerv <149519589+PrincessLaSwerv@users.noreply.github.com>
2024-04-17 16:29:56 -0600

    Unvalidated Inputs are functioning main.asm
    Unvalidated inputs (HH:MM) can be accepted still which needs to be fixed, the values will be stored as variables Start_HH , Start_MM , End_HH, End_MM 
    In order to call these values to manipulate we want to LD [R~] Variable

--- main.asm
+++ main.asm
@@ -2,123 +2,93 @@
 ; When you see a '^' it is a comment reffering to the line above
 ;       
 .ORIG x3000
+            AND   R1, R1, #0 ; Clear R1
+            AND   R2, R2, #0 ; Clear R2
+            LD    R3, CONVERT_ASC_TO_DEC ; Converts ASCII to Decimal within Register (#-48)
        
-       AND   R1, R1, #0 ; Clear R1
-       AND   R2, R2, #0 ; Clear R2
-       LD    R3, ASCII ; Load ASCII difference
+START       LEA   R0, PROMPT_START ; Load prompt for start time
+            JSR   GET_CHAR
+            ST    R2, Start_HH    ;Saving Start (HH:--) after return 
+            ST    R5, Start_MM    ;Saving Start (--:MM) after return 
+
+            LEA   R0, PROMPT_END   ; Load prompt for end time
+            JSR   GET_CHAR
+            ST    R2, End_HH    ;Saving End (HH:--) after return 
+            ST    R5, End_MM    ;Saving End (--:MM) after return 
+            
+            
+
        
-START  LEA   R0, PROMPT_START ; Load prompt for start time
-       JSR   GET_CHARACTER
-       ;TRAP  x22               ; Display prompt
-       ;TRAP  x23               ; Request keyboard input for start time
-       ;ADD   R1, R0, #0       ; Store input into R1
-       ;ST   R0, START_TIME    ; Store R1 into the START_TIME BLOCK
-       ; ^ SHOULD STORE THE INPUT FROM USER INTO START_TIME BLOCK
+EXIT        TRAP X25   ; HALT
 
-       LEA   R0, PROMPT_END   ; Load prompt for end time
-       JSR   GET_CHARACTER
-       ;TRAP  x22               ; Display prompt
-       ;TRAP  x23               ; Request keyboard input for end time
-       ;ADD   R1, R0, #0    ;
-       ;ST   R0, END_TIME      ; Store end time input in memory  ; THIS IS NOT WORKING 
-       ; ^ SHOULD STORE THE INPUT FROM USER INTO END_TIME BLOCK
-
-
-       ; BELOW IS TESTING TO MAKE SURE WE ARE ABLE TO PULL STORED INPUTS
-       ;LEA   R0, START_TIME ; Load address of start time
-       ;TRAP  x22            ; Display address
-       ; ^ above will dispaly the input character
-       
-       ;LD    R0, START_TIME ; Load content of start time
-       ;TRAP  x22            ; Display content
-       ; ^ this displays ascii characters of the input so its just question marks
-       
-       ;LEA   R0, END_TIME ; Load address of end time
-       ;TRAP  x22          ; Display address
-       ; ^ above will dispaly the input character
-       
-       
-       ;LD    R0, END_TIME ; Load content of end time
-       ;TRAP  x22          ; Display content
-       ; ^ this displays ascii characters of the input so its just question marks
-
-
-       ; BELOW IS WHERE WE ARE STARTING PREPERATION FOR CALCULATION 
-       ; PROBABLY WON'T WORK NORMALLY SINCE THEY ARE GOING TO INPUT AS TIME FORMAT
-       
-       ; RIGHT NOW JUST TRYING TO GET THE R2, TO BE SUBTRACTED TO R1 CORRECTLY AND OUTPUT A NUMBER
-       ;LD   R1, START_TIME   ; Load start time input into R1 
-       ;NOT   R1, R1           ; Should negate start time so we can add them
-       ;ADD   R1, R1, #1       ; 2's complement adjustment
-       ;ADD   R1, R1, R3       ; Adjust for ASCII conversion
-       
-
-       ;LD   R2, END_TIME     ; Load end time
-       ;NOT   R2, R2           ; Negate end time
-       ;ADD   R2, R2, #1       ; 2's complement adjustment
-       ;ADD   R2, R2, R3       ; Adjust for ASCII conversion
-
-
-       ;ADD   R1, R2, R1       ; Calculate R2 - R1
-       ; Convert the result back to ASCII characters
-       ;ADD  R0, R1, #0         ; Copy the result to R0
-       ; ^NOT SURE IF WE SHOULD ADD ASCII TO IT INSTEAD OF JUST 0 MAYBE SOMETHING TO TRY
-       ;TRAP x21                ; Display the result
-
-       ;BRnzp EXIT             ; Exit the program
-       
-
-ERROR  LEA   R0, ERRORMSG ; load error message
-       TRAP  X22      ; Output error message
-       
-EXIT   TRAP X25   ; HALT
-
-GET_CHARACTER   PUTS
-GETC
-OUT
-ADD     R2,R0, #0   ; Store value into R2 
-ADD     R2,R2,R3    ; Converting to ASCII R2 = "A"
-GETC
-OUT
-ADD     R4,R0,#0    ; Store value into R3
-ADD     R4,R4,R3    ;Converting to ASCII R4 = "B"
-GETC
-OUT
-ADD     R5,R0, #0   ; Store value into R5
-ADD     R5,R5,R3    ; Converting to ASCII R5 = "C"
-GETC
-OUT
-ADD     R6,R0,#0    ; Store value into R6
-ADD     R6,R6,R3    ;Converting to ASCII R6 = "D"
-JSR     CONVERT_TENS_PLACE
-RET
-
-CONVERT_TENS_PLACE
-LD R1, TEN
-AND R3,R3, #0           ;Clear R3
+GET_CHAR    PUTS
+            GETC
+            OUT
+            ADD     R2,R0, #0   ; Store value into R2 
+            ADD     R2,R2,R3    ; Converting to ASCII R2 = "A"
+            GETC
+            OUT
+            ADD     R4,R0,#0    ; Store value into R3
+            ADD     R4,R4,R3    ;Converting to ASCII R4 = "B"
+            GETC
+            OUT
+            ADD     R5,R0, #0   ; Store value into R5
+            ADD     R5,R5,R3    ; Converting to ASCII R5 = "C"
+            GETC
+            OUT
+            ADD     R6,R0,#0    ; Store value into R6
+            ADD     R6,R6,R3    ;Converting to ASCII R6 = "D"
+            ;JSR     CONVERT_TENS_PLACE
+        
+            LD      R1, TEN
+            ST      R4, SaveR4
+            AND     R4,R4, #0   ;Clear R4
+            
 MULTITENA:
-        AND R3,R3,R2   ;Add tens value to itself 10 times
-        ADD R1,R1, #-1  ;decreasing counter
-        BRp MULTITENA    ;Continue loop until 0
-AND R2, R2, #0  ;Clear R2
-ADD R2,R2,R3    ;Move result to R2
-AND R3,R3, #0
+            ADD     R4,R4,R2   ;Add tens value to itself 10 times
+            ADD     R1,R1, #-1  ;decreasing counter
+            BRp     MULTITENA    ;Continue loop until 0
+            
+            AND     R2, R2, #0  ;Clear R2
+            ADD     R2,R2,R4    ;Move R4 result back to R2 
+            LD      R4, SaveR4  ;Loading original value for "B"
+            ADD     R2 , R2 , R4 ;Adding "A" tens place and "B" result in R2 
+            ;R2 Successfully holds the hour "(HH:--)" portion
+            
+            LD      R1, TEN
+            AND     R4,R4, #0   ;Clear R4
 MULTITENC:
-        AND R3,R3,R5   ;Add tens value to itself 10 times
-        ADD R1,R1, #-1  ;decreasing counter
-        BRp MULTITENC    ;Continue loop until 0
-AND R5, R5, #0  ;Clear R5
-ADD R5,R5,R3    ;Move result to R5
-AND R3, R3, #0  ; Clear R3 again
-LD  R3, ASCII   ; Load ASCII value
-RET
+            ADD     R4,R4,R5   ;Add tens value to itself 10 times
+            ADD     R1,R1, #-1  ;decreasing counter
+            BRp     MULTITENC    ;Continue loop until 0
+            
+            AND     R5, R5, #0  ;Clear R5
+            ADD     R5,R5,R4    ;Move result to R5 
+            ADD     R5, R5 , R6 ; Adding "C" and "D" together 
+            ; R5 Successfully holds the minutes "(--:MM)" Portion
+            RET
 
 
-ASCII  .FILL x0020
-START_TIME .BLKW 2
-END_TIME   .BLKW 2
-PROMPT_START .STRINGZ "Enter start time (HH:MM): "
-PROMPT_END   .STRINGZ "\nEnter end time (HH:MM): "
-ERRORMSG .STRINGZ "Not a valid input"
-TEN     .FILL  #10
-.END
+CONVERT_ASC_TO_DEC  .FILL #-48
+ASCII	            .FILL #48 
+TEN                 .FILL  #10
+
+START_TIME          .BLKW 2
+END_TIME            .BLKW 2
+PROMPT_START        .STRINGZ "Enter start time using military Time(HH:MM): "
+PROMPT_END          .STRINGZ "\nEnter end time using military Time(HH:MM): "
+ERRORMSG            .STRINGZ "Not a valid input"
+
+SaveR1              .FILL   x0000
+SaveR2              .FILL   x0000
+SaveR3              .FILL   x0000
+SaveR4              .FILL   x0000
+SaveR5              .FILL   x0000
+SaveR6              .FILL   x0000
+Start_HH            .FILL   x0000
+Start_MM            .FILL   x0000
+End_HH              .FILL   x0000
+End_MM              .FILL   x0000
+
+
+            .END
